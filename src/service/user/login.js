e45a261534a386876c922d7e54e3fba3d8b4b6df
Dungndph20697 <dungthoai321@gmail.com>
2025-11-11 10:58:06 +0700

    thêm đăng nhập thất bại

---
+++ src/service/user/login.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { apiUserLogin, apiFindUserByToken } from "../../config/api.js";
+
+const login = async (username, pass) => {
+  try {
+    const response = await axios.post(apiUserLogin, {
+      username: username,
+      password: pass,
+    });
+
+    if (response.data) {
+      localStorage.setItem("token", response.data.token);
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Login failed:", error);
+    return "Tên đăng nhập hoặc mật khẩu không đúng";
+  }
+};
+
+const findUserByToken = async (token) => {
+  try {
+    const response = await axios.get(apiFindUserByToken, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error.response?.data?.message;
+  }
+};
+
+export { login, findUserByToken };
