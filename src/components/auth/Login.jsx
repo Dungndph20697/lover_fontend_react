7f42ec73a057bd5083aa76181f6afaab9eb51cbc
Dungndph20697 <dungthoai321@gmail.com>
2025-11-11 09:34:27 +0700

    thêm thư viện

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,19 @@
+export default function Login() {
+  return (
+    <>
+      <h2>Login Page</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
